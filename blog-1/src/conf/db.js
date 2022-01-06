ca8c7ec0739f41aded753636be7bb59e23e75b12
yocai <277240773@qq.com>
2022-01-06 17:47:23 +0800

    node读写文件

--- blog-1/src/conf/db.js
+++ blog-1/src/conf/db.js
@@ -5,6 +5,7 @@
 const env = process.env.NODE_ENV // 获取环境变量
 
 let MYSQL_CONF
+let REDIS_CONF
 
 if (env === 'dev') { // 开发环境数据库配置
   MYSQL_CONF = {
@@ -13,6 +14,10 @@ if (env === 'dev') { // 开发环境数据库配置
     password: 'chanyocai',
     port: '3306',
     database: 'myblog'
+  }
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0,1'
   }
 }
 
@@ -23,6 +28,10 @@ if (env === 'production') { // 生产环境数据库配置
     password: 'chanyocai',
     port: '3306',
     database: 'yocai'
+  }
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0,1'
   }
 }
 
