cb86fe5aa96f16e4f9cb2b9c51f7438f34c790fe
yocai <277240773@qq.com>
2022-01-13 15:33:33 +0800

    password encryption

--- blog-1/src/controller/user.js
+++ blog-1/src/controller/user.js
@@ -1,7 +1,10 @@
 // 引入 sql 执行函数
 const { exec, escape } = require('../db/mysql')
+const { genPassword } = require("../utils/cryp")
 
 const login = (username, password) => {
+  // 生成加密密码
+  password = genPassword(password)
   username = escape(username)
   password = escape(password)
   const sql = `
