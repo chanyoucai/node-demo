a4d4249703c5a3b35294d54d84aa3f622c76dbde
yocai <277240773@qq.com>
2022-01-13 11:33:01 +0800

    prevent sql injection

--- blog-1/src/controller/user.js
+++ blog-1/src/controller/user.js
@@ -1,9 +1,11 @@
 // 引入 sql 执行函数
-const { exec } = require('../db/mysql')
+const { exec, escape } = require('../db/mysql')
 
 const login = (username, password) => {
+  username = escape(username)
+  password = escape(password)
   const sql = `
-    select username from users where username='${username}' and password='${password}';
+    select username from users where username=${username} and password=${password};
   `
   return exec(sql).then(rows => {
     return rows[0] || []
