f3d9f761f43da0de30604af7bac685f724ef28cd
chanyoucai <277240773@qq.com>
2022-01-18 23:54:18 +0800

    install npm package

--- blog-express/app.js
+++ blog-express/app.js
@@ -5,26 +5,26 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 // 引用
 const blogRouter = require('./routes/blog');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // 注册路由
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 // 访问规则：若访问 blogRouter 中的 list，
 // 则为 localhost:3000/api/blog/list
 app.use('/api/blog', blogRouter);
