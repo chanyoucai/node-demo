ce109eb305729cb8ef24ce23d50c8de9fa1be255
yocai <277240773@qq.com>
2022-01-19 11:11:15 +0800

    blog-express 接口基本编写

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -1,10 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+const {
+  getList,
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog
+} = require('../controller/blog')
 
-router.get('/list', function(req, res, next) {
-  res.json({
-    errno: 0,
-    data: [1,2,3]
+router.get('/list', function (req, res, next) {
+  // res.json({
+  //   errno: 0,
+  //   data: [1,2,3]
+  // })
+  // express 会直接把 query 参数整合在 req.query 中
+  let author = req.query.author || ''
+  let keyword = req.query.keyword || ''
+  // if (req.query.isAdmin) {
+  //   // 管理员
+  //   const loginCheckResult = loginCheck(req)
+  //   if (loginCheckResult) {
+  //     // 未登录
+  //     return loginCheckResult
+  //   }
+  //   // 查询自己的博客列表
+  //   author = req.session.username
+  // }
+  const result = getList(author, keyword)
+  result.then(listData => {
+    res.json(
+      new SuccessModel(listData)
+    )
   })
 });
 
